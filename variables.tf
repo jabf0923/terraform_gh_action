1abd7045e277dc5f6eb85dda33606dcb22244a62
Alex Balderas <62255537+jabf0923@users.noreply.github.com>
2022-07-07 22:19:15 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "aws secret key"
+  type        = string
+}
+variable "AWS_ACCESS_KEY_ID" {
+  description = "aws access key"
+  type        = string
+}
